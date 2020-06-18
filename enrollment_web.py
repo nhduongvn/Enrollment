07043cefbf5184823f8319e1cda3a123ddb44e6a
Duong Hai Nguyen <nhduongvn1@gmail.com>
2020-06-18 09:08:44 -0400

    update

--- enrollment_web.py
+++ enrollment_web.py
@@ -4,15 +4,18 @@ import matplotlib.pyplot as plt
 @st.cache
 def load_data(nrows):
   data = pd.read_csv('Data/predictionTest.csv')
+  data = data.loc[data['ed']>100]
+  data.drop(index=data.loc[data['District_Name']=='Adams-Cheshire'].index,inplace=True)
   return data
 
 dat = load_data(10000)
-dat.sort_values(by=['district'],ascending=True,inplace=True)
-district_name_all = dat['district'].values
+#print(dat.loc[dat['District_Name']=='Adams-Cheshire'])
+dat.sort_values(by=['District_Name'],ascending=True,inplace=True)
+district_name_all = dat['District_Name'].values
 district_name_all_list = list(set(district_name_all))
 district_name_all_list.sort()
 district_name_all_list.insert(0,"")
-print(district_name_all_list)
+#print(district_name_all_list)
 
 
 st.header('Budget planning for economically disadvantaged students in Massachusetts school districts')
@@ -24,14 +27,20 @@ st.subheader('Budget amount')
 #district_name = st.text_input("District name", "")
 district_name = st.sidebar.selectbox("District name", district_name_all_list)
 #ed_pre = dat.loc[(dat['district']==district_name) & (dat['schoolyear']=='2018-19')]['ed_li_pct_pred']
-ed_pre = dat.loc[(dat['district']==district_name) & (dat['schoolyear']=='2020-21')]['bg_ed_only']
-print(dat.loc[(dat['district']==district_name) & (dat['schoolyear']=='2020-21')]['bg_ed_only'])
+ed_pre = dat.loc[(dat['District_Name']==district_name) & (dat['schoolyear']=='2020-21')]['bg_ed_only']
+#print(dat.loc[(dat['district']==district_name) & (dat['schoolyear']=='2020-21')]['bg_ed_only'])
+#dat1 = dat.loc[dat['District_Name']=='Andover']
+#print(dat1)
 if district_name != "":
-  #st.write('Budget amount for ED students in 2020-2021: $' + str(ed_pre.values[0]))
+  st.write('Budget amount for ED students in 2020-2021: $' + str(int(ed_pre.values[0]*1000000)))
   #dat.loc[dat['district']==district_name].plot.bar(x='schoolyear',y='ell_pct')
   #st.write('Percentage of enrolled economic disadvantage over year: ')
-  ax = dat.loc[dat['district']==district_name].plot(x="schoolyear", y=['bg_norm','bg_with_ed'], kind="barh")
-  ax.legend(['Budget without additional ED support','Budget with ED support'])
+  dat1 = dat.loc[dat['District_Name']==district_name]
+  dat1.sort_values(by=['schoolyear'],inplace=True)
+  #ax = dat1.plot(x="schoolyear", y=['bg_norm','bg_with_ed'], kind="barh")
+  #ax.legend(['Budget without additional ED support','Budget with ED support'],loc='center left')
+  ax = dat1.plot(x="schoolyear", y=['bg_ed_only'], kind="barh")
+  ax.legend(['Budget for ED student support'],loc='center left')
   ax.set_xlabel('Budget amount ($1M)')
   ax.set_ylabel('School year')
   st.pyplot()
